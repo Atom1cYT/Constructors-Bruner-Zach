3362bcd2e5b13198e729829513ef5a1cd96d23b1
Atom1cYT <146125539+Atom1cYT@users.noreply.github.com>
2024-04-11 10:05:33 -0500

    gg

---
+++ Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors_Bruner_Zach
+{
+    internal class Game
+    {
+
+        public string title, genre;
+        // uses the tile and genre to input into the command prompt Unknown title and Casaul 
+        public Game(string titleParam = "Unknown Title", string genreParam = "Casual")
+        {
+            title = titleParam;
+            genre = genreParam;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿    using System;
+
+namespace Constructors_Bruner_Zach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Puts CS:GO is an FPS game into the system 
+            Game myGame = new Game("CS:GO", "FPS");
+            // Inputs MYfavortie game is my game title and it is a my game genre into the system to be replaced BY CS:GO and FPS
+           Console.WriteLine($"My favorite game is {myGame.title} and it is a {myGame.genre} game!");
+            
+            // inputs the other game and new game into the system 
+            Game myOtherGame = new Game();
+            // Inputs into system 
+            Console.WriteLine($"My other favorite game is {myOtherGame.title} and it is a {myOtherGame.genre} game!");
+        }
+    }
+}
